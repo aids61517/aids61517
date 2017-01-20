4e0f81aaa5384a8adc6d01f059e5fc96d03aff9f
deming huang <deming_huang@hiiir.com>
2017-01-20 17:04:12 +0800

    update test function

--- javascripts/testWebView.js
+++ javascripts/testWebView.js
@@ -5,6 +5,10 @@ function fuck2(){
 	alley.onClick("自行call Alley")
 }
 function getResult() {
-	var x = document.getElementById("resultText");
-	return x.innerHTML;
+	// var x = document.getElementById("resultText");
+	return 3;
+}
+function getResult1() {
+	// var x = document.getElementById("resultText");
+	return "33";
 }
